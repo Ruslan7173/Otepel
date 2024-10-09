cf7e61f357cbdb4927924717c91e1dc8eb69a93d
Ruslan7173 <lifecol57@gmail.com>
2024-10-09 19:59:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+     int[] massiv = {-20, 30, -40, 50, 10, -10};
+       
+
+
+
+    }
+public static int met(int[]massiv){
+    int max = 0;
+    int c = 0;
+
+    for (int i = 0; i < massiv.length; i++) {
+        if (massiv[i]<1){
+            c = 0;
+}else { c=c+1;
+
+        }
+        if (c>max){
+            max =c;
+
+            }
+
+
+
+
+
+return max;
+    }
+    public static void
+}
